ce3a150ebd625d9ae9fec2f7c565e94c08c610a3
Lance Jeffers <lancej1022@gmail.com>
2020-02-20 16:34:54 -0800

    cool

---
+++ build/precache-manifest.c9f15024fac592754f080fde9cd1310d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78c591eb1db3f4e42298cfab78634e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf8832a9755b19a12786",
+    "url": "/static/css/main.2e788ee5.chunk.css"
+  },
+  {
+    "revision": "43a1c3c942d9b6b96ead",
+    "url": "/static/js/2.0fbcfc8a.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/static/js/2.0fbcfc8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8832a9755b19a12786",
+    "url": "/static/js/main.fc215bb5.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
