43849f170c783b292215370584d5099913a181c1
Lance Jeffers <lancej1022@gmail.com>
2020-02-20 16:54:00 -0800

    cool2

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,25 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import './Input.css';
 
-const Input = ({ message, setMessage, sendMessage }) => {
-    return (
-        <form className="form">
-            <input
-                className="input"
-                type="text"
-                placeholder="Type a message..."
-                value={message}
-                onChange={({ target: { value } }) => setMessage(value)}
-                onKeyPress={event =>
-                    event.key === 'Enter' ? sendMessage(event) : null
-                }
-            />
-            <button className="sendButton" onClick={e => sendMessage(e)}>
-                Send
-            </button>
-        </form>
-    );
-};
+const Input = ({ setMessage, sendMessage, message }) => (
+    <form className="form">
+        <input
+            className="input"
+            type="text"
+            placeholder="Type a message..."
+            value={message}
+            onChange={({ target: { value } }) => setMessage(value)}
+            onKeyPress={event =>
+                event.key === 'Enter' ? sendMessage(event) : null
+            }
+        />
+        <button className="sendButton" onClick={e => sendMessage(e)}>
+            Send
+        </button>
+    </form>
+);
 
 export default Input;
